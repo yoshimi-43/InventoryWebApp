733d385be92751715fdd4899509db6e08fb212f6
yoshi <irieyoshimi1@gmail.com>
2025-10-09 15:15:31 +0900

    バリデーション追加

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,16 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace InventoryWebApp.Models
 {
     public class Product
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "商品名は必須です。")]
         public string Name { get; set; } = string.Empty;
 
+        [Range(0, int.MaxValue, ErrorMessage = "数量は0以上で入力してください。")]
         public int Quantity { get; set; }
 
-        public decimal Price { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "価格は1以上で入力してください。")]
+        public int Price { get; set; }
 
         // 自動計算プロパティ
-        public decimal TotalValue => Quantity * Price;
+        public int TotalValue => Quantity * Price;
     }
 }
